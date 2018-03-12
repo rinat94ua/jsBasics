0855159d7cf7708dda6e0b8ffd080879dacfa036
vodo4ka <rinat94ua@gmail.com>
2018-03-13 01:36:50 +0300

    Challange 2

---
+++ challange2/script.js
@@ -0,0 +1,11 @@
+var years = [1999, 1292, 2212, 1223, 2333];
+
+var personsYear = [];
+
+for (var i = 0; i < years.length; i++) {
+	persons.push(years[i]);
+}
+
+for (var i = 0; i < years.length; i++) {
+	
+}
--- script.js
+++ script.js
@@ -279,7 +279,45 @@ console.log(john);
 
 
 /////////////////////////
-//// Lecture: Loops	//
+//// Lecture: Loops	////
+/*
+for (var i = 0; i < 10; i++) {
+	console.log(i);
+}
+
+
+var names = ['john', 'mark', 'bob', 'jane', 'mary'];
+
+for (var i = 0; i < names.length; i++) {
+	console.log(names[i]);
+}
+
+for (var i = names.length - 1; i >= 0; i--) {
+	console.log(names[i]);
+}
+
+var names = ['john', 'mark', 'bob', 'jane', 'mary'];
+
+var i = 0;
+while (i < names.length) {
+	console.log(names[i]);
+	i++;
+}
+*/
+
+/////////////////////////
+//// Lecture: Loops	////
+
+
+
+
+
+
+
+
+
+
+
 
 
 
