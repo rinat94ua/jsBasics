98028dd1393a8298b43e02e9fae4552929b330ab
vodo4ka <rinat94ua@gmail.com>
2018-03-13 16:26:52 +0300

    Lecture 34 - Events

--- 4. JavaScript in the Browser DOM Manipulation and Events/35. Download the Code/4-DOM-Pig-Game/app.js
+++ 4. JavaScript in the Browser DOM Manipulation and Events/35. Download the Code/4-DOM-Pig-Game/app.js
@@ -8,19 +8,38 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 1;
 
-dice = Math.floor(Math.random() * 6 + 1);
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
-//document.querySelector('#current-' + activePlayer).innedHTML = '<em>' + dice + '</em>';
-
-
 var x = document.querySelector('#score-0').textContent;
 console.log(x);
 
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+	//1. Random number
+	var dice = Math.floor(Math.random() * 6 + 1);
+
+	//2. Display result
+	var diceDOM = document.querySelector('.dice');
+	diceDOM.style.display = 'block';
+	diceDOM.src = 'dice-' + dice + '.png';
+
+	//3. Update the round score IF the rolled number is not a 1
+	if (dice !== 1) {
+		//Add score
+		roundScore += dice;
+		document.querySelector('#current-' + activePlayer).textContent = roundScore;
+	} else {
+		//Next player
+		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+	}
+});
