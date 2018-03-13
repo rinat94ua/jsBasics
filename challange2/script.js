fb8495035f4ec0661e677bafd5e56b4be139419a
vodo4ka <rinat94ua@gmail.com>
2018-03-13 13:29:56 +0300

    Lecture 20 - ES5, ES6/ES2015 and ES2016

--- challange2/script.js
+++ challange2/script.js
@@ -1,11 +1,21 @@
-var years = [1999, 1292, 2212, 1223, 2333];
+function printFullAge (years) {
+	var personsYear = [];
+	var moreThan18 = [];
 
-var personsYear = [];
+	for (var i = 0; i < years.length; i++) {
+		personsYear.push(years[i]);
+	}
 
-for (var i = 0; i < years.length; i++) {
-	persons.push(years[i]);
+	for (var i = 0; i < years.length; i++) {
+		if (2018 - years[i] > 18) {
+			moreThan18.push(true);
+		} else {
+			moreThan18.push(false);
+		}
+	}
+	return moreThan18;
 }
 
-for (var i = 0; i < years.length; i++) {
-	
-}
+var years = [1999, 1292, 2212, 1223, 2001];
+
+console.log(printFullAge(years));
