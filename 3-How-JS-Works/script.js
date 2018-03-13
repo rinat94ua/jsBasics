b47c5369964229564f81ee24801417bb9e4d48e1
vodo4ka <rinat94ua@gmail.com>
2018-03-13 14:32:38 +0300

    Lecture 26 - Scoping

--- 3-How-JS-Works/script.js
+++ 3-How-JS-Works/script.js
@@ -1,16 +1,32 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
+/*
+//functions
+calculateAge(1965);
+function calculateAge(year) {
+  console.log(2016 - year);
+}
 
+//calculateAge(1990);
 
+//retirement(1990);
 
+var retirement = function(year) {
+  console.log(65 - (2016 - year));
+}
 
+//variables
+console.log(age);
+var age = 23;
 
+function foo() {
+  var age = 65;
+  console.log(age);
+}
 
-
-
-
-
-
+foo();
+console.log(age);
+*/
 
 
 
