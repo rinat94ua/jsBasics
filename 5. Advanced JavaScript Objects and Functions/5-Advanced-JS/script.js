aa59ea3bcd40ce85c2cb172e2fc2d5fb6bae477b
vodo4ka <rinat94ua@gmail.com>
2018-03-14 15:34:16 +0300

    Lecture 51 - Immediately invoked function expressions (IIFE)

--- 5. Advanced JavaScript Objects and Functions/5-Advanced-JS/script.js
+++ 5. Advanced JavaScript Objects and Functions/5-Advanced-JS/script.js
@@ -128,15 +128,36 @@ console.log(rates);
 */
 
 
-//
+// Functions: returning functions
+/*
+function interviewQuestion(job) {
+	if (job === 'designer') {
+		return function(name) {
+			console.log(name + ', can you explain what design is?');
+		};
+	} else if (job === 'teacher') {
+		return function(name) {
+			console.log('What subject do you teach, ' + name + '?');
+		};
+	} else {
+		return function(name) {
+			console.log('Hello ' + name + ', what do you do?');
+		};
+	}
+};
 
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
 
+teacherQuestion('John');
+designerQuestion('Jane');
+designerQuestion('Mark');
+designerQuestion('Mike');
 
+interviewQuestion('teacher')('Mark');
+*/
 
-
-
-
-
+// Immediately invoked function expressions (IIFE)
 
 
 
