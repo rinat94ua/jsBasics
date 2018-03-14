3776807e47353a7b7382bde4e64c9d8d7c09f216
vodo4ka <rinat94ua@gmail.com>
2018-03-14 16:33:57 +0300

    Lecture 60 - Bind, call and apply methods

--- 5. Advanced JavaScript Objects and Functions/5-Advanced-JS/script.js
+++ 5. Advanced JavaScript Objects and Functions/5-Advanced-JS/script.js
@@ -165,7 +165,6 @@ function game() {
 }
 game();
 
-
 (function () {
 	var score = Math.random() * 10;
 	console.log(score >= 5);
@@ -180,7 +179,54 @@ game();
 */
 
 
-// Closures
+////////////////
+// Closures ///
+/*
+function retirement(retirementAge) {
+	var a = ' years left until retirement.';
+
+	return function(yearOfBirth) {
+		var age = 2016 - yearOfBirth;
+		console.log((retirementAge - age) + a);
+	}
+}
+
+var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementGermany(1990);
+retirementIceland(1990);
+retirementUS(1990);
+
+//retirement(66)(1990);
+
+
+function interviewQuestion(job) {
+	return function(name) {
+		if (job === 'designer') {
+			console.log(name + ', can you explain what design is?');
+		} else if (job === 'teacher') {
+			console.log('What subject do you teach, ' + name + '?');
+		} else {
+			console.log('Hello ' + name + ', what do you do?');
+		}
+	}
+}
+
+interviewQuestion('teacher')('John');
+*/
+
+//////////////////////////////
+//// Bind, Call, Apply //////
+
+
+
+
+
+
+
+
 
 
 
