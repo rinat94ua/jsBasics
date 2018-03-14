5a052caffc87b4c4c73c12bac89f5440d1f61c71
vodo4ka <rinat94ua@gmail.com>
2018-03-14 14:26:14 +0300

    Lecture 50 - First class functions

--- 5. Advanced JavaScript Objects and Functions/5-Advanced-JS/script.js
+++ 5. Advanced JavaScript Objects and Functions/5-Advanced-JS/script.js
@@ -34,7 +34,7 @@ console.log(mark.lastName);
 */
 
 //Object.create
-
+/*
 var personProto = {
 	caclAge: function() {
 		console.log(2016 - this.yearOfBirth);
@@ -51,6 +51,47 @@ var jane = Object.create(personProto, {
 	yearOfBirth: { value: 1990 },
 	job: { value: 'designer' }
 });
+*/
+
+// Primitives vs Objects
+
+//Primitives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+//Objects
+var obj1 = {
+	name: 'John',
+	age: 26
+}
+
+var obj2 = obj1;
+obj1.age = 30;
+
+console.log(obj1.age);
+console.log(obj2.age);
+
+//Function
+var age = 27;
+var obj = {
+	name: 'Jonas',
+	city: 'Lisbon'
+};
+
+function change(a, b) {
+	a = 30;
+	b.city = 'San Francisco';
+}
+
+change(age, obj);
+
+console.log(age);
+console.log(obj.city);
+
+
 
 
 
